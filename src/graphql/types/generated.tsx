1cfd5cd46c0e0020f84f792852088110333d6af3
Martin Richter <me@martinrichter.net>
2019-07-06 01:05:07 +0300

    Update GraphQL typings with latest schema

--- src/graphql/types/generated.tsx
+++ src/graphql/types/generated.tsx
@@ -27,23 +27,41 @@ export type Appearance = {
   instrumentName: Scalars["String"];
   /** The full name of the appearance's participant. */
   participantName: Scalars["String"];
+  /** The appearance's result, if publicly available. */
+  result?: Maybe<Result>;
 };
 
 export type Contest = {
   __typename?: "Contest";
+  /** The contest categories offered at this contest. */
+  contestCategories: Array<ContestCategory>;
   /** The country code of the contest's host. */
   countryCode: Scalars["String"];
   /** The dates on which the contest is happening. */
   dates: Array<Scalars["Date"]>;
-  /** The last day of the contest. */
-  endDate: Scalars["Date"];
   id: Scalars["ID"];
   /** The contest’s name containing the round, year and host. */
   name: Scalars["String"];
   /** The stages used in this contest. */
   stages: Array<Stage>;
-  /** The first day of the contest. */
-  startDate: Scalars["Date"];
+};
+
+export type ContestCategory = {
+  __typename?: "ContestCategory";
+  id: Scalars["ID"];
+  /** The contest category's name. */
+  name: Scalars["String"];
+  /** The amount of performances with public results in this contest category. */
+  publicResultCount: Scalars["Int"];
+};
+
+export type Host = {
+  __typename?: "Host";
+  /** The country code of the host. */
+  countryCode: Scalars["String"];
+  id: Scalars["ID"];
+  /** The name of the host. */
+  name: Scalars["String"];
 };
 
 export type Performance = {
@@ -53,6 +71,10 @@ export type Performance = {
   /** The performance's contest category and age group. */
   categoryInfo: Scalars["String"];
   id: Scalars["ID"];
+  /** The performance's pieces. */
+  pieces: Array<Piece>;
+  /** The host of the performance's predecessor contest. */
+  predecessorHost?: Maybe<Host>;
   /** The scheduled wall time of the performance. */
   stageTime: Scalars["Time"];
 };
@@ -62,12 +84,39 @@ export type PerformanceFilter = {
   stageId?: Maybe<Scalars["ID"]>;
 };
 
+export type Piece = {
+  __typename?: "Piece";
+  id: Scalars["ID"];
+  /** For classical pieces, this contains the name and biographical dates of the piece's composer.
+   * For popular pieces, the artist name is returned.
+   */
+  personInfo: Scalars["String"];
+  /** The title of the piece. */
+  title: Scalars["String"];
+};
+
+export type Result = {
+  __typename?: "Result";
+  /** Whether the participant will advance to the next round with this appearance. */
+  advances: Scalars["Boolean"];
+  /** The points awarded to this appearance. */
+  points: Scalars["Int"];
+  /** The prize corresponding to the appearance's points. */
+  prize?: Maybe<Scalars["String"]>;
+};
+
 export type RootQueryType = {
   __typename?: "RootQueryType";
   /** The contests with public timetables. */
   contests: Array<Contest>;
-  /** The performances of a contest. */
+  /** A single performance that's scheduled in a public contest. */
+  performance?: Maybe<Performance>;
+  /** The scheduled performances of a public contest. */
   performances?: Maybe<Array<Performance>>;
+};
+
+export type RootQueryTypePerformanceArgs = {
+  id: Scalars["ID"];
 };
 
 export type RootQueryTypePerformancesArgs = {
