1bfcabf4fbc6520004feb0ec8ba11be2143595c6
Martin Richter <me@martinrichter.net>
2019-07-06 01:02:44 +0300

    Update GraphQL URL to reflect merged backend PR

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,7 +1,7 @@
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-  uri: "https://jumubase-staging-pr-13.herokuapp.com/graphql"
+  uri: "https://jumubase-staging.herokuapp.com/graphql"
 });
 
 export default client;
