d045a845165400278a2d9c4292153df72a6c00cd
Martin Richter <martin_ext@biowink.com>
2020-03-02 11:33:00 +0100

    Set up API key and base URL via environment vars

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,9 +1,11 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { API_KEY, API_URI } from "react-native-dotenv";
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({
-    uri: "https://jumubase-staging.herokuapp.com/graphql",
+    uri: API_URI,
+    headers: { "X-Api-Key": API_KEY },
   }),
   defaultOptions: {
     watchQuery: {
